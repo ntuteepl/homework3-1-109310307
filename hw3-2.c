11692fd27414a2d999bf6b28922a33fd3344bfe5
109310307 <146065460+109310307@users.noreply.github.com>
2023-10-19 18:58:11 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -3,9 +3,6 @@
 
 main()
 {
-
-//hw3-2
-
 int s1,d1,s2,d2,s3,d3;
 scanf("%d %d %d %d %d %d", &s1, &d1, &s2, &d2, &s3, &d3);
 int car = 3;
@@ -27,13 +24,13 @@ if(0 <= s1,d1,s2,d2,s3,d3 <= 24)
     else if(d2 <= s1)
     {
         car = car -1;
-        if( (d2 <= s3 & d3 <= s1) | (d1 <= s3) | (d3 <= s1) )
+        if( (d2 <= s3 & d3 <= s1) | (d1 <= s3) | (d3 <= s2) )
             car = car -1;
     }
     else if(d2 <= s3)
     {
         car = car -1;
-        if( (d2 <= s1 & d1 <= s3) | (d3 <= s1) | (d1 <= s2)  )
+        if( (d2 <= s1 & d1 <= s3) | (d3 <= s2) | (d1 <= s3)  )
             car = car -1;
     }
     else if(d3 <= s1)
@@ -51,5 +48,5 @@ if(0 <= s1,d1,s2,d2,s3,d3 <= 24)
 
 }
 printf("%d",car);
-
 }
+
